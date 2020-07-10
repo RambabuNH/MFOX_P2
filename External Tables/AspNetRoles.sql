dcfb9ec1a2879731071c4426afa73c66bb47e022
RambabuNH <58956866+RambabuNH@users.noreply.github.com>
2020-07-10 23:56:20 +0000

    Add files via upload

---
+++ External Tables/AspNetRoles.sql
@@ -0,0 +1,8 @@
+﻿CREATE EXTERNAL TABLE [dbo].[AspNetRoles] (
+    [Id] NVARCHAR (128) NOT NULL,
+    [Name] NVARCHAR (256) NOT NULL
+)
+    WITH (
+    DATA_SOURCE = [MyElasticDBQueryDataSrc]
+    );
+
---
+++ External Tables/AspNetUserRoles.sql
@@ -0,0 +1,8 @@
+﻿CREATE EXTERNAL TABLE [dbo].[AspNetUserRoles] (
+    [UserId] NVARCHAR (128) NOT NULL,
+    [RoleId] NVARCHAR (128) NOT NULL
+)
+    WITH (
+    DATA_SOURCE = [MyElasticDBQueryDataSrc]
+    );
+
